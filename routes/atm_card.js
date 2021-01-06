c023cbf239969e153419663524256989d0489d0d
Alan Biju <67945306+Alan-Biju@users.noreply.github.com>
2021-01-06 21:47:59 +0530

    CVV

--- routes/atm_card.js
+++ routes/atm_card.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express();
+const { sqlconnection } = require('../database/mysql');
+
 
 
 router.get('/card',(req, res)=>{
-    res.render('atm_card',{account:req.cookies.account,name:req.cookies.name});
+sqlconnection.query(`SELECT * FROM card WHERE account_no=${req.cookies.account} `,(error,result)=>{
+if(!error){
+    
+ res.render('atm_card',{account:result[0].account_no,name:req.cookies.name,cvv:result[0].CVV});
+}
+else{
+    console.log('card error');
+}
+   
+});
+   
 });
 
 
 module.exports = router;
+
