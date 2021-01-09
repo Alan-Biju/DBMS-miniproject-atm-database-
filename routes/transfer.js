ad5087d44e82c558fbb6564904ccba693a8380e7
Alan Biju <67945306+Alan-Biju@users.noreply.github.com>
2021-01-10 00:05:47 +0530

    time zone upadate

--- routes/transfer.js
+++ routes/transfer.js
@@ -2,7 +2,31 @@ const express = require('express');
 const router = express();
 const { sqlconnection } = require('../database/mysql');
 
+//-------------------------------
+function taskDate2(dateMilli) {
+    var d = (new Date(dateMilli) + '').split(' ');
+    d[2] = d[2] + ',';
+  
+    return [d[0], d[1], d[2], d[3]].join(' ');
+  }
+  
+  const dates=taskDate2(new Date());
 
+
+
+const d = new Date();
+const localTime = d.getTime();
+const localOffset = d.getTimezoneOffset() * 60000;
+const utc = localTime + localOffset;
+const offset = 5.5;  
+const bombay = utc + (3600000*offset);
+const nd = new Date(bombay);
+
+  const time = nd.toLocaleTimeString() ; 
+
+
+
+//------------------------------
 
 router.get('/transfer',(req, res)=>{
    res.render('transfer',{error:""});
@@ -17,12 +41,12 @@ router.post('/transfer',(req, res)=>{
         
         if(Number(req.cookies.balance)>=amount){
             sqlconnection.query(`SELECT * FROM balance WHERE account_no=${account}`,(error,results)=>{
-                if(!error){
+                if(results.length>0){
                  var balance = Number(results[0].balance);
                     const rec_balance=eval(balance+amount);
                    sqlconnection.query(`UPDATE balance SET balance =${rec_balance} WHERE account_no =${account}`,(error,results)=>{
                        if(!error){
-                            sqlconnection.query(`INSERT INTO deposit(account_no,type,amount) VALUES(${account},'Received',${amount})`,(error,results)=>{
+                            sqlconnection.query(`INSERT INTO deposit(account_no,type,amount,date,time) VALUES(${account},'Received',${amount},"${dates}","${time}")`,(error,results)=>{
                                 if(!error){
                                     console.log("deposit enterd")
                                 }
@@ -34,7 +58,7 @@ router.post('/transfer',(req, res)=>{
                         sqlconnection.query(`UPDATE balance SET balance=${send_balance} WHERE account_no=${Number(req.cookies.account)}`,(error,results)=>{
                             if(!error){
                                 
-                                sqlconnection.query(`INSERT INTO withdrawal(account_no,type,amount) VALUES(${req.cookies.account},'Transferred',${Number(amount)})`,(error,results)=>{
+                                sqlconnection.query(`INSERT INTO withdrawal(account_no,type,amount,date,time) VALUES(${req.cookies.account},'Transferred',${Number(amount)},"${dates}","${time}")`,(error,results)=>{
                                     if(!error){
                                         res.clearCookie('balance');
                                       
