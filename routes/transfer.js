d55213bdb6f0bae21d9d19fa6da75fc55dd8e7f2
Alan Biju <67945306+Alan-Biju@users.noreply.github.com>
2021-01-10 14:15:53 +0530

    time setup

--- routes/transfer.js
+++ routes/transfer.js
@@ -3,27 +3,7 @@ const router = express();
 const { sqlconnection } = require('../database/mysql');
 
 //-------------------------------
-function taskDate2(dateMilli) {
-    var d = (new Date(dateMilli) + '').split(' ');
-    d[2] = d[2] + ',';
-  
-    return [d[0], d[1], d[2], d[3]].join(' ');
-  }
-  
-  const dates=taskDate2(new Date());
-
-
-
-const d = new Date();
-const localTime = d.getTime();
-const localOffset = d.getTimezoneOffset() * 60000;
-const utc = localTime + localOffset;
-const offset = 5.5;  
-const bombay = utc + (3600000*offset);
-const nd = new Date(bombay);
-
-  const time = nd.toLocaleTimeString() ; 
-
+const {time,date,today}=require('../routes/time');
 
 
 //------------------------------
@@ -46,7 +26,7 @@ router.post('/transfer',(req, res)=>{
                     const rec_balance=eval(balance+amount);
                    sqlconnection.query(`UPDATE balance SET balance =${rec_balance} WHERE account_no =${account}`,(error,results)=>{
                        if(!error){
-                            sqlconnection.query(`INSERT INTO deposit(account_no,type,amount,date,time) VALUES(${account},'Received',${amount},"${dates}","${time}")`,(error,results)=>{
+                            sqlconnection.query(`INSERT INTO deposit(account_no,type,amount,date,time) VALUES(${account},'Received',${amount},"${date}","${time}")`,(error,results)=>{
                                 if(!error){
                                     console.log("deposit enterd")
                                 }
@@ -58,7 +38,7 @@ router.post('/transfer',(req, res)=>{
                         sqlconnection.query(`UPDATE balance SET balance=${send_balance} WHERE account_no=${Number(req.cookies.account)}`,(error,results)=>{
                             if(!error){
                                 
-                                sqlconnection.query(`INSERT INTO withdrawal(account_no,type,amount,date,time) VALUES(${req.cookies.account},'Transferred',${Number(amount)},"${dates}","${time}")`,(error,results)=>{
+                                sqlconnection.query(`INSERT INTO withdrawal(account_no,type,amount,date,time) VALUES(${req.cookies.account},'Transferred',${Number(amount)},"${date}","${time}")`,(error,results)=>{
                                     if(!error){
                                         res.clearCookie('balance');
                                       
