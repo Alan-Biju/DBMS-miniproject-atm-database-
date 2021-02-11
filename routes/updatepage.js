07900f6664eb888f66fa52e370f8778b376b95ef
Alan Biju <67945306+Alan-Biju@users.noreply.github.com>
2021-02-11 22:09:58 +0530

    Update updatepage.js

--- routes/updatepage.js
+++ routes/updatepage.js
@@ -3,15 +3,31 @@ const router = express.Router();
 const { sqlconnection } = require('../database/mysql');
 
 router.get('/update', (req, res) => {
-  res.render('updatepage',{account:req.cookies.account});
+  var details_account =
+    'SELECT * FROM details WHERE account_id=' + req.cookies.account + ';';
+  sqlconnection.query(details_account, (error, results) => {
+    if (error) {
+      console.log('somthing worng in details table ' + error);
+      res.redirect('/dashboard');
+    } else {
+      res.render('updatepage', {
+        name: results[0].name,
+        password: results[0].pin,
+        email: results[0].email,
+        address: results[0].address,
+        phone: results[0].phone,
+        Dob: results[0].birth,
+        account: req.cookies.account,
+      });
+    }
+  });
 });
 
 router.post('/update', (req, res) => {
   const { username, dob, email, password, phone, address } = req.body;
- 
 
   const card = `update card set pin=${password} where account_no=${req.cookies.account}`;
-  const detail = `update details set pin=${password},name='${username}',address='${address}',email='${email}',birth=${dob},phone=${phone} where account_id=${req.cookies.account}`;
+  const detail = `update details set pin=${password},name='${username}',address='${address}',email='${email}',birth='${dob}',phone=${phone} where account_id=${req.cookies.account}`;
   sqlconnection.query(card, (error, results) => {
     if (!error) {
       res.clearCookie('name');
@@ -19,15 +35,16 @@ router.post('/update', (req, res) => {
 
       sqlconnection.query(detail, (error, results) => {
         if (!error) {
-            sqlconnection.query(`update balance set pin_no=${password} where account_no=${req.cookies.account}`,(error,results)=>{
-                    if(!error){
-                        res.redirect('/account');
-                    }
-                    else{
-                        console.log('error' + error);
-                    }
-            });
-          
+          sqlconnection.query(
+            `update balance set pin_no=${password} where account_no=${req.cookies.account}`,
+            (error, results) => {
+              if (!error) {
+                res.redirect('/account');
+              } else {
+                console.log('error' + error);
+              }
+            }
+          );
         } else {
           console.log('error' + error);
         }
